f895154376807b8f57426fe8fc38fcd40a616bbd
jin ruan <jruan6377@bths.edu>
2014-03-12 21:20:41 -0400

    Initial commit

---
+++ index.py
@@ -0,0 +1,11 @@
+__author__ = 'user'
+print("Your first python program!!!")
+
+foo = input('What is your name: ')
+print('Nice to meet you ' + foo)
+
+yoMom = "I like big data"
+if yoMom == "I like big data":
+    print(yoMom + 'is true')
+else:
+    print ('is not true')
